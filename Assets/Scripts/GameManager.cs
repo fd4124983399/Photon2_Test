369f083a017dc7c5f20c9cad2dc183cc339f9545
NaI <fd4124983399@gmail.com>
2020-05-21 15:45:08 +0800

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.ABCDE.MyApp
+{
+    public class GameManager : MonoBehaviourPunCallbacks
+    {
+        [Tooltip("Prefab- 玩家的角色")]
+        public GameObject playerPrefab;
+
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            if (playerPrefab == null)
+            {
+                Debug.LogError("playerPrefab 遺失, 請在 Game Manager 重新設定",
+                    this);
+            }
+            else
+            {
+                PhotonNetwork.Instantiate(this.playerPrefab.name,
+                    new Vector3(0f, 0f, 260f), Quaternion.identity, 0);
+            }
+        }
+
+
+        // 玩家離開遊戲室時, 把他帶回到遊戲場入口
+        public override void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("{0} 進入遊戲室", other.NickName);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("我是 Master Client 嗎? {0}",
+                    PhotonNetwork.IsMasterClient);
+                LoadArena();
+            }
+        }
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("{0} 離開遊戲室", other.NickName);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("我是 Master Client 嗎? {0}",
+                    PhotonNetwork.IsMasterClient);
+                LoadArena();
+            }
+        }
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("我不是 Master Client, 不做載入場景的動作");
+            }
+            Debug.LogFormat("載入{0}人的場景",
+                PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+namespace Com.ABCDE.MyApp
+{
+    public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
+    {
+        void Update()
+        {
+            if (photonView.IsMine)
+            {
+                ProcessInputs();
+            }
+        }
+
+        void ProcessInputs()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.position += Vector3.left * 50 * Time.deltaTime;
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * 50 * Time.deltaTime;
+            }
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsWriting)
+            {
+                // 為玩家本人的狀態, 將 IsFiring 的狀態更新給其他玩家
+                stream.SendNext(transform.position);
+            }
+            else
+            {
+                // 非為玩家本人的狀態, 單純接收更新的資料
+                transform.position = (Vector3)stream.ReceiveNext();
+            }
+        }
+    }
+}
